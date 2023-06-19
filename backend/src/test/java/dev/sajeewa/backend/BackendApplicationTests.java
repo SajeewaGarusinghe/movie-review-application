70d0151c495c7d2d45cf0a768e94f939ae3a73c2
sajeewa garusinghe <sajeeva616@gmail.com>
2023-06-19 10:42:16 +0530

    starting of new backend

---
+++ backend/src/test/java/dev/sajeewa/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sajeewa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
